709ccd6919fb9cb1005a1b3aa4ee62b5c142d455
Mohit Singh <mohitbanna085@gmail.com>
2026-02-16 13:29:37 +0530

    react project

--- FollowCount/src/App.jsx
+++ FollowCount/src/App.jsx
@@ -1,78 +1,51 @@
-import { ChevronLeft } from 'lucide-react';
-import { UserPlus } from 'lucide-react';
-import './App.css'
-import { useState } from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
+const App = () => {
+  const [follow, setFollow] = useState(false);
+  const [followers, setFollowers] = useState(66868);
 
-function App() {
-
-  const [follow, setFollow] = useState(563)
-  const [isFollowing, setIsFollowing] = useState(false)
-
-   const handleFollow = () => {
-
-    if (isFollowing) {
-      setFollow(follow - 1)
+  const handleFollow = () => {
+    if (follow) {
+      setFollowers(prev => prev - 1);
     } else {
-      setFollow(follow + 1)
+      setFollowers(prev => prev + 1);
     }
-
-    setIsFollowing(!isFollowing)
-  }
+    setFollow(!follow);
+  };
 
   return (
-    <div>
-
-      <div className='container'>
-
-        <div className='otrbox'>
-          <p><ChevronLeft /></p>
-          <div className='topbar'>
-
-            <p className='username'>rendom123</p>
-            <img src="https://img.freepik.com/premium-vector/blue-bold-instagram-verification-icon_525160-5705.jpg?w=360" alt="" />
-          </div>
-          <p>...</p>
-
+    <div className="main-container">
+      <div className="card">
+        <div className="left">
+          <div className="profile-pic">200 × 200</div>
         </div>
-
-        <div className='otrbox'>
-          <div className='profileimg'></div>
-
-          <div className='populer'>
+            
+        <div className="right">
+          
+ <div className="user">
+             <h2>Chrisse</h2>
+          <p className="address">4018 Sachs Trail</p>
+           </div>
+          <div className="stats">
             <div>
-              <h6>12</h6>
-              <p>posts</p>
+              <h3>Posts</h3>
+              <p>1841</p>
             </div>
 
             <div>
-              <h6>{follow}</h6>
-              <p>Followers</p>
-            </div>
-
-            <div>
-              <h6>1</h6>
-              <p>Following</p>
+              <h3>Followers</h3>
+              <p>{followers}</p>
             </div>
           </div>
         </div>
 
-       
-
-        <div className='otrbox'>
-          <button className='follow' onClick={handleFollow}>
-            {isFollowing ? "following" : "Follow"}
-          </button>
-          <button className='msg'>message</button>
-          <div className='icon'>
-            <UserPlus className='iconinr' />
-          </div>
-        </div>
-
+        <button className="follow-btn" onClick={handleFollow}>
+          {follow ? "Following" : "Follow"}
+        </button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
